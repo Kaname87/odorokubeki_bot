467c724849304d24894c771153229b87ce6c3f3b
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-11 03:37:45 +0800

    loop every line to address issue for invalid tag case

--- article_finder.rb
+++ article_finder.rb
@@ -6,11 +6,10 @@ require './const'
 
 def read_and_extract(filename, search_word)
   results = []
-  # CSV出力用に改行削除
-  # また、一部不正な改行文字がテキストに含まれてしまっており、適切なパースの邪魔をするため、それも削除
-  articles = File.read(filename).gsub(/[\r\n]/, '').gsub(/<(BR|br)>/, '')
 
-  # wikipediaの出力ファイルには複数のXML エレメント(doc エレメント)が並列して書かれており、全てをネストするルートエレメントが不在
+  articles = read_and_clean(filename)
+
+  # Wikipediaの出力ファイルには複数のXML エレメント(doc エレメント)が並列して書かれており、全てをネストするルートエレメントが不在
   # この場合Nokogiriは一番先頭のXMLエレメントとその子供しかパースしてくれないので、
   # ルートエレメントを追加して、全体をvalidなXMLにする
   xml = "<root>#{articles}</root>"
@@ -28,6 +27,37 @@ def read_and_extract(filename, search_word)
     results << result
   end
   results
+end
+
+def read_and_clean(filename)
+  # CSV出力用に改行削除
+  # また、一部不正な改行タグがテキストに含まれてしまっており、適切なパースの邪魔をするため、それも削除
+  # <証明> や <明日天気になあれ> など特殊な<>の使用方があるため、すべて別の文字に置き換える。
+  # docタグのみ残す
+  all_file_content = ''
+  File.readlines(filename).each do |line|
+    text = line.gsub(/[\r\n]/, '').gsub(/<(BR|br)>/, '')
+    next if text.empty?
+
+    matched = text.match(/<.+>/)
+
+    if matched.nil?
+      all_file_content += text
+      next
+    end
+
+    raise 'Unexpected Case' if matched.length > 1
+
+    has_tag_text = matched[0]
+    all_file_content += if has_tag_text =~ /^<doc/ || has_tag_text =~ %r{/doc>$}
+                          # doc タグはそのまま
+                          text
+                        else
+                          # doc 以外で<>を使ってる場合、別の記号に変更
+                          has_tag_text.tr('<', '[').tr('>', ']')
+                        end
+  end
+  all_file_content
 end
 
 def append_to_csv(filename, results)
