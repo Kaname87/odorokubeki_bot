1a7fb5014d3247738930dae7ae5e8ef4c1fdf365
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-10 23:35:23 +0800

    Make TweetCreator class

--- tweet_creator.rb
+++ tweet_creator.rb
@@ -1,130 +1,134 @@
-require "csv"
-require "./const"
+# frozen_string_literal: true
 
-def calc_max_sentence_length(row)
-    MAX_TWEET_LENGTH - (row[TITLE_IDX].length + row[ID_IDX].length + URL_BASE.length)
-end
+require 'csv'
+require './const'
 
-def extract_sentence(content, max_sentence_length)
+class TweetCreator
+  ID_COLUMN_IDX = 0
+  TITLE_COULMN_IDX = 1
+  CONTENT_COLUMN_IDX = 2
+
+  OUTPUT_TEXT = 'text.txt'
+
+  def initialize(source_filename)
+    @rows = CSV.foreach(source_filename).drop(1)
+  end
+
+  def pick_random_one
+    tartget_row = rondom_select_row
+    tweet_text = create_tweet_text(tartget_row)
+  end
+
+  def output_all_to_file
+    tweet_texts = []
+    @rows.each do |row|
+      tweet_texts << create_tweet_text(row)
+    end
+    File.open(OUTPUT_TEXT, 'w') do |f|
+      f.puts(tweet_texts.join("\n"))
+    end
+  end
+
+  private
+
+  def calc_max_sentence_length(row)
+    MAX_TWEET_LENGTH - (row[TITLE_COULMN_IDX].length + row[ID_COLUMN_IDX].length + URL_BASE.length)
+  end
+
+  def extract_sentence(content, max_sentence_length)
     # 文で区切るパターン
     sentence_by_period = extract_sentence_by_period(content, max_sentence_length)
-    if (sentence_by_period.length <= max_sentence_length)
-        return sentence_by_period
+    if sentence_by_period.length <= max_sentence_length
+      return sentence_by_period
     end
 
     # 括弧で区切るパターン
     sentence_by_parentheses = extract_sentence_by_parenthese(content, max_sentence_length)
-    if (sentence_by_parentheses.length <= max_sentence_length)
-        return sentence_by_parentheses
+    if sentence_by_parentheses.length <= max_sentence_length
+      return sentence_by_parentheses
     end
 
     # どちらも長すぎたら、長さで
-    return extract_sentence_by_length(content, max_sentence_length)
-end
+    extract_sentence_by_length(content, max_sentence_length)
+  end
 
-def extract_sentence_by_period(content, max_sentence_length)
+  def extract_sentence_by_period(content, _max_sentence_length)
     keyword_idx = content.index(KEY_WORD)
     # keyword前と後に分割
     first_part = content[0...keyword_idx]
     last_part = content[keyword_idx..-1]
 
-    period = "。";
+    period = '。'
 
     # Sentence のstart index
     start_idx = first_part.rindex(period)
-    if start_idx == nil
-        start_idx = 0 
+    if start_idx.nil?
+      start_idx = 0
     else
-        start_idx += 1 # period charcterを含まない為に+1
+      start_idx += 1 # period charcterを含まない為に+1
     end
 
     # Sentence の end index
-    end_idx = last_part.index(period) 
-    if end_idx == nil
-        end_idx = last_part.length
-    end
+    end_idx = last_part.index(period)
+    end_idx = last_part.length if end_idx.nil?
 
     first_part_sentence = first_part[start_idx..-1]
 
     last_part_sentence = last_part[0..end_idx]
 
-    return first_part_sentence + last_part_sentence
-end
+    first_part_sentence + last_part_sentence
+  end
 
-def sentence()
-end
-
-def extract_sentence_by_parenthese(content, max_sentence_length)
+  def extract_sentence_by_parenthese(content, _max_sentence_length)
     keyword_idx = content.index(KEY_WORD)
     # keyword前と後に分割
     first_part = content[0...keyword_idx]
     last_part = content[keyword_idx..-1]
 
-    start_parenthese = "「";
-    end_parenthese = "」";
+    start_parenthese = '「'
+    end_parenthese = '」'
 
     # Sentence のstart index
     start_idx = first_part.rindex(start_parenthese)
-    if start_idx == nil
-        start_idx = 0 
-    end
+    start_idx = 0 if start_idx.nil?
 
     # Sentence の end index
     end_idx = last_part.index(end_parenthese)
-    if end_idx == nil
-        end_idx = last_part.length
-    end
+    end_idx = last_part.length if end_idx.nil?
 
     first_part_sentence = first_part[start_idx..-1]
     last_part_sentence = last_part[0..end_idx]
 
-    return first_part_sentence + last_part_sentence
-end
+    first_part_sentence + last_part_sentence
+  end
 
-def extract_sentence_by_length(content, max_sentence_length)
+  def extract_sentence_by_length(content, max_sentence_length)
     keyword_idx = content.index(KEY_WORD)
     ellipsis = '...'
 
     end_idx = keyword_idx + max_sentence_length - ellipsis.length
-    return content[keyword_idx...end_idx] + ellipsis
-end
+    content[keyword_idx...end_idx] + ellipsis
+  end
 
-def create_tweet_text(row)
+  def create_tweet_text(row)
     max_sentence_length = calc_max_sentence_length(row)
-    sentence = extract_sentence(row[CONTENT_IDX], max_sentence_length)
+    sentence = extract_sentence(row[CONTENT_COLUMN_IDX], max_sentence_length)
 
-    text = <<-TWEET
-#{sentence}
+    text = <<~TWEET
+      #{sentence}
 
-- #{row[TITLE_IDX]}
-#{URL_BASE}#{row[ID_IDX]}
+      - #{row[TITLE_COULMN_IDX]}
+      #{URL_BASE}#{row[ID_COLUMN_IDX]}
     TWEET
 
-    return text
-end
+    text
+  end
 
-def output_to_file()
-    tweet_texts = []
-    CSV.foreach(KEYWORD_ARTICLES_CSV_NAME).drop(1).each do |row|
-        tweet_texts << create_tweet_text(row)
-    end
-    File.open('text.txt', 'w') do |f|
-        f.puts(tweet_texts.join("\n"))
-    end
-end
-
-def rondom_select_row
-    rows = CSV.foreach(KEYWORD_ARTICLES_CSV_NAME).drop(1)
-    last_idx = (rows.length) -1
-    
+  def rondom_select_row
+    last_idx = @rows.length - 1
     target_idx = Random.rand(0..last_idx)
-    
-    return rows[target_idx]
+    @rows[target_idx]
+  end
 end
 
-def pickone
-    tartget_row = rondom_select_row
-    tweet_text = create_tweet_text(tartget_row)
-end
-
-
+p TweetCreator.new(KEYWORD_ARTICLES_CSV_NAME).pick_random_one if $PROGRAM_NAME == __FILE__
