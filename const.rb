13e39607978b2910820f98b6b19e3c4d79143f11
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-10 23:37:30 +0800

    Remove unnecessary const

--- const.rb
+++ const.rb
@@ -1,11 +1,9 @@
+# frozen_string_literal: true
+
 KEY_WORD = '驚くべきことに'
 
 MAX_TWEET_LENGTH = 130
 
 KEYWORD_ARTICLES_CSV_NAME = 'result.csv'
-
-ID_IDX = 0
-TITLE_IDX = 1
-CONTENT_IDX = 2
 
 URL_BASE = 'https://ja.wikipedia.org/wiki?curid='
