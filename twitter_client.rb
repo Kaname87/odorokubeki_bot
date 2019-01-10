726a5c8a7db0b315cbeb91bf771bf09415cb9733
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-11 00:59:33 +0800

    Use dotenv gem instead for env vars

--- twitter_client.rb
+++ twitter_client.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
+require 'dotenv'
 require 'twitter'
-
-require './keys'
 require './tweet_creator.rb'
 
 class TwitterClient
   def initialize
+    Dotenv.load
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = YOUR_CONSUMER_KEY
-      config.consumer_secret     = YOUR_CONSUMER_SECRET
-      config.access_token        = YOUR_ACCESS_TOKEN
-      config.access_token_secret = YOUR_ACCESS_SECRET
+      config.consumer_key        = ENV['YOUR_CONSUMER_KEY']
+      config.consumer_secret     = ENV['YOUR_CONSUMER_SECRET']
+      config.access_token        = ENV['YOUR_ACCESS_TOKEN']
+      config.access_token_secret = ENV['YOUR_ACCESS_SECRET']
     end
   end
 
   def post(text)
     @client.update(text)
+    p 'posted'
   rescue StandardError => e
     p e
   end
-  p 'posted'
 end
